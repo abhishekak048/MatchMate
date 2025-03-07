8fcff32ac687b4a7dd64a2294a2d06ba68d97723
Abhishek kumar <54079908+abhishekak048@users.noreply.github.com>
2025-03-07 11:33:44 +0530

    Initial Commit

---
+++ MatchMateProject/MatchMateProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMateProjectApp.swift
+//  MatchMateProject
+//
+//  Created by Abhishek Kumar on 07/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MatchMateProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
